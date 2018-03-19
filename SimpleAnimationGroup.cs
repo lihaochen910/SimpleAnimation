89605174994a3fede5082c00fac3e5266cd9dadb
Kanbaru <lihaochen910@hotmail.com>
2018-03-19 11:09:09 +0800

    添加动画预设配置组 修改源,让动画预设组可以在运行时替换

---
+++ SimpleAnimationGroup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableItem/SimpleAnimationGroup")]
+public class SimpleAnimationGroup : ScriptableObject
+{
+    [SerializeField]
+    private SimpleAnimation.EditorState[] _animationStates;
+
+    public SimpleAnimation.EditorState[] AnimationStates
+    {
+        get { return _animationStates; }
+        set
+        {
+            if (value != null) _animationStates = value;
+        }
+    }
+}
